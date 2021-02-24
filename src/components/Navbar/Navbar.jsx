f96145ed1c233a3cca829075ff9b82b8dd22fc5f
procy-dev <procy.dev@gmail.com>
2021-02-24 14:24:07 -0800

    Added product carts and commerce-js instance to pull products from api

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles';
+import Logo from '../../assets/logo.png'
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        <img src={Logo} alt="Commerce.js" height="50px" className={classes.image} />
+                        Procy Commerce
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart item" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
+
+export default Navbar;
